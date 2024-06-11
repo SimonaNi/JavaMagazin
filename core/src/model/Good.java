0e4f49bd0b4c02c45d9aa1f04b4d305d602419fc
Sss <125960911+SimonaNi@users.noreply.github.com>
2024-06-11 12:26:40 +0300

    BIgDecimal +

--- core/src/model/Good.java
+++ core/src/model/Good.java
@@ -12,7 +12,7 @@ public abstract class Good {
     private LocalDate expirationDate;
     private int quantity;
 
-    public Good(String id, String name, double unitDeliveryPrice, String category, LocalDate expirationDate, int quantity) {
+    public Good(String id, String name, BigDecimal unitDeliveryPrice, String category, LocalDate expirationDate, int quantity) {
         this.id = id;
         this.name = name;
         this.unitDeliveryPrice = unitDeliveryPrice;
@@ -29,7 +29,7 @@ public abstract class Good {
         return name;
     }
 
-    public double getUnitDeliveryPrice() {
+    public BigDecimal getUnitDeliveryPrice() {
         return unitDeliveryPrice;
     }
 
--- core/src/model/NonFoodGood.java
+++ core/src/model/NonFoodGood.java
@@ -3,14 +3,18 @@ package model;
 import util.DateUtil;
 
 public class NonFoodGood extends Good {
-    private static final double MARKUP_PERCENTAGE = 0.30; // 30%
+    private static final double MARKUP_PERCENTAGE = 0.30;
 
     public NonFoodGood(String id, String name, double unitDeliveryPrice, LocalDate expirationDate, int quantity) {
         super(id, name, unitDeliveryPrice, "Non-Food", expirationDate, quantity);
     }
 
     @Override
-    public double getSellingPrice(int daysUntilExpiration) {
-        return getUnitDeliveryPrice() * (1 + MARKUP_PERCENTAGE);
-    }
+    public BigDecimal getSellingPrice(int daysUntilExpiration) {
+        BigDecimal markupPrice = getUnitDeliveryPrice().multiply(BigDecimal.valueOf(1 + MARKUP_PERCENTAGE));
+        if (daysUntilExpiration < DISCOUNT_DAYS) {
+            markupPrice = markupPrice.multiply(BigDecimal.valueOf(1 - DISCOUNT_PERCENTAGE));
+        }
+        return markupPrice;
+
 }
